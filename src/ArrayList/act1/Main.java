923343c1915d3069909342eadb40d31c0e51b686
Ivan <ivaye2@alu.edu.gva.es>
2026-02-11 09:41:32 +0100

    Implementacion del try-catch y arreglo de bugs

--- src/ArrayList/act1/Main.java
+++ src/ArrayList/act1/Main.java
@@ -11,79 +11,93 @@ public class Main {
         boolean volverImp = true;
         String name;
         String phoneNumber;
-        while (!salir) {
-            if (volverImp) {
-                System.out.print("""
-                        
-                        1. Imprimir contacto
-                        2. add contacto
-                        3. update contacto
-                        4. remove contacto
-                        5. find contacto
-                        6. volver a imprimir la lista de opciones
-                        
-                        """);
-                volverImp = false;
-            }
-            menu = sc.nextInt();
-            sc.nextLine();
+        try {
+            while (!salir) {
+                if (volverImp) {
+                    System.out.print("""
+                            
+                            1. Imprimir contacto
+                            2. Add contacto
+                            3. Update contacto
+                            4. Remove contacto
+                            5. Find contacto
+                            6. Volver a imprimir la lista de opciones
+                            
+                            """);
+                    volverImp = false;
+                }
+                System.out.print("Introduce un valor para el menu: ");
+                menu = sc.nextInt();
+                sc.nextLine();
 
-            switch (menu) {
-                case 0:
-                    salir = true;
-                    break;
-                case 1:
-                    System.out.println();
-                    telefonoMovil.printContacts();
-                    break;
-                case 2:
-                    System.out.print("Nombre: ");
-                    name = sc.nextLine();
-                    System.out.print("\nTelefono: ");
-                    phoneNumber = sc.nextLine();
-
-                    telefonoMovil.addNewContact(Contacto.createContact(name, phoneNumber));
-
-                    break;
-
-                case 3:
-                    System.out.print("Nombre: ");
-                    name = sc.nextLine();
-
-                    if (telefonoMovil.queryContact(name) != null) {
-                        System.out.print("Nuevo Nombre: ");
-                        String newName = sc.nextLine();
-                        System.out.print("\nNuevo Telefono: ");
+                switch (menu) {
+                    case 0:
+                        salir = true;
+                        break;
+                    case 1:
+                        System.out.println();
+                        telefonoMovil.printContacts();
+                        break;
+                    case 2:
+                        System.out.print("Nombre: ");
+                        name = sc.nextLine();
+                        System.out.print("\nTelefono: ");
                         phoneNumber = sc.nextLine();
-                        telefonoMovil.updateContact(telefonoMovil.queryContact(name), Contacto.createContact(newName, phoneNumber));
-                    } else
-                        System.out.printf("el valor '%s' no existe\n", name);
-                    break;
-                case 4:
-                    System.out.print("Nombre: ");
-                    name = sc.nextLine();
-                    if (telefonoMovil.removeContact(telefonoMovil.queryContact(name))) {
-                        telefonoMovil.removeContact(telefonoMovil.queryContact(name));
-                        System.out.println("\nSe ha eliminado correctamente");
-                    } else
-                        System.out.printf("el valor '%s' no existe\n", name);
-                    break;
-                case 5:
-                    System.out.print("Nombre: ");
-                    name = sc.nextLine();
-                    Contacto contacto = telefonoMovil.queryContact(name);
-                    if (telefonoMovil.queryContact(name) != null)
-                            System.out.printf("Nombre: %s \n Numero de telefono: %s", name, contacto.getPhoneNumber());
-                    else
-                        System.out.printf("el valor '%s' no existe\n", name);
-                    break;
-                case 6:
-                    volverImp = true;
-                    break;
 
-                default:
-                    throw new IllegalStateException("Unexpected value: " + menu);
+                        if(telefonoMovil.addNewContact(Contacto.createContact(name, phoneNumber)))
+                            telefonoMovil.addNewContact(Contacto.createContact(name, phoneNumber));
+
+                        else
+                            System.out.printf("El valor '%s' no existe\n", name);
+                        break;
+
+                    case 3:
+                        System.out.print("Nombre: ");
+                        name = sc.nextLine();
+
+                        if (telefonoMovil.queryContact(name) != null) {
+                            System.out.print("Nuevo Nombre: ");
+                            String newName = sc.nextLine();
+                            System.out.print("\nNuevo Telefono: ");
+                            phoneNumber = sc.nextLine();
+                            if(telefonoMovil.addNewContact(Contacto.createContact(name, phoneNumber))) {
+                                telefonoMovil.updateContact(telefonoMovil.queryContact(name), Contacto.createContact(newName, phoneNumber));
+                                System.out.println("Se ha actualizado correctamente");
+                            }
+                            else
+                                System.out.printf("El valor '%s' ya existe existe\n", newName);
+                        } else
+                            System.out.printf("El valor '%s' no existe\n", name);
+                        break;
+                    case 4:
+                        System.out.print("Nombre: ");
+                        name = sc.nextLine();
+                        if (telefonoMovil.removeContact(telefonoMovil.queryContact(name))) {
+                            telefonoMovil.removeContact(telefonoMovil.queryContact(name));
+                            System.out.println("\nSe ha eliminado correctamente");
+                        } else
+                            System.out.printf("El valor '%s' no existe\n", name);
+                        break;
+                    case 5:
+                        System.out.print("Nombre: ");
+                        name = sc.nextLine();
+                        Contacto contacto = telefonoMovil.queryContact(name);
+                        if (telefonoMovil.queryContact(name) != null)
+                            System.out.printf("Nombre: %s \n Numero de telefono: %s", name, contacto.getPhoneNumber());
+                        else
+                            System.out.printf("el valor '%s' no existe\n", name);
+                        break;
+                    case 6:
+                        volverImp = true;
+                        break;
+
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + menu);
+                }
             }
+        }
+        catch(IllegalStateException e){
+            System.out.println(e.getMessage());
         }
     }
 }
